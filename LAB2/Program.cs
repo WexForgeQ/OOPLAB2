828b41945a0eed0b66a59cb66956adfe0dd61099
WexForgeQ <95540615+WexForgeQ@users.noreply.github.com>
2022-10-07 15:24:01 +0300

    Update Program.cs
    Добавлены свойства для всех полей, доработан конструктор с параметрами по умолчанию

--- LAB2/Program.cs
+++ LAB2/Program.cs
@@ -2,15 +2,126 @@
 
 class House
 {
-    private const ushort maxexpl = 100;
+    private const ushort maxexpl = 2022;
+    public ushort MaxExpl
+    {
+        get
+        {
+            return maxexpl;
+        }
+        private set
+        {
+            value = 100; ;
+        }
+
+    }
     private short? explDate;
+    public short? Expl
+    {
+        get
+        {
+            if (explDate > 0)
+                return explDate;
+            else
+            {
+                Console.WriteLine("Неверно введедна дата эксплуатации");
+                explDate = null;
+                return explDate;
+            }       
+        }
+        set
+        {explDate = value;}
+    }
     private short? hsnumber;
+    public short? Hsn
+    {
+        get
+        {
+            if (hsnumber > 0)
+                return hsnumber;
+            else
+            {
+                Console.WriteLine("Неверно введен номер дома");
+                hsnumber = null;
+                return hsnumber;
+            }
+        }
+        set
+        {
+            hsnumber = value;
+        }
+    }
     private static int id;
     private short? floatn, floor, roomn;
-    private string street;
+    public short? Flat
+    {
+        get
+        {
+            if (floatn > 0)
+                return floor;
+            else
+            {
+                Console.WriteLine("Неверно введен номер квартиры");
+                floatn = null;
+                return floatn;
+            }
+        }
+        set
+        {
+            floatn = value;
+        }
+    }
+    public short? Floor
+    {
+        get
+        {
+            if (floor > 0 && floor <10)
+                return floor;
+            else
+            {
+                Console.WriteLine("Неверно введен номер этажа");
+                floor = null;
+                return floor;
+            }
+        }
+        set
+        {
+            floor = value;
+        }
+    }
+    public short? Room
+    {
+        get
+        {
+            if (roomn > 0)
+                return roomn;
+            else
+            {
+                Console.WriteLine("Неверно введен номер дома");
+                roomn = null;
+                return roomn;
+            }
+        }
+        set
+        {
+           roomn = value;
+        }
+    }
+    private string? street;
+    public string? Street
+    {
+        get
+        {
+            return street;
+        }
+        set
+        {
+            street = value;
+        }
+    }
     private static string bltype;
-    static int counter = 1;
-    private static readonly int hashid;
+    static int counter = 0;
+    private static int hashid;
 
     public House(short hsn, short flt, short flr, short rmn, string str, short expl)
     {
@@ -35,12 +146,24 @@ class House
             explDate = expl;
         else
             Console.WriteLine($"Время экспулатации дома {id} введёно некоректно(<0)");
+        id = counter++;
+    }
+    public House(string str="undf")
+    {
+        hsnumber = null;
+        floatn = null;
+        floor = null;
+        roomn = null;
+        explDate = null;
+        street = str;
+        id = counter++;
     }
     static House()
     {
-        id = counter++;
+        
         hashid = id.GetHashCode();
         bltype = "9-floor";
+        id = counter++;
     }
     private House(){}
     public void DisplayHouse()
@@ -51,20 +174,24 @@ class House
         Console.WriteLine("Этаж: "+ floor);
         Console.WriteLine("Кол-во комнат: "+ roomn);
         Console.WriteLine("Улица: "+ street);
-        if(explDate<maxexpl)
-        {
-            Console.WriteLine("Время эксплуатации: " + explDate);
-        }
-        else
-        {
-            Console.WriteLine("Время эксплуатации: " + explDate+"(Дому необходим капитальный ремонт)");
-        }
+        Console.WriteLine("Время эксплуатации: " + explDate);
         Console.WriteLine("Тип дома: "+ bltype);
         Console.WriteLine("Код дома: "+ hashid);
     }
 
+    public override bool Equals(object obj)
+    {
+        return base.Equals(obj);
+    }
 
-
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+    public override string ToString()
+    {
+        return base.ToString();
+    }
 
 
 }
@@ -75,11 +202,14 @@ namespace LAB2
 {
     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
             House A = new House(-1, 2, 0, 3, "ba", 101);
             A.DisplayHouse();
-
+            House B = new House();
+            B.DisplayHouse();
+            A.Floor = 4;
+            Console.WriteLine(A.Floor);
 
 
 
